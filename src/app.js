e90696f8ecfa6be9e09558bd7c961ff94d263fae
Nicolas Suarez <nicosube2014@gmail.com>
2023-01-03 20:27:55 -0300

    Finalizacion TP-14

--- src/app.js
+++ src/app.js
@@ -4,27 +4,31 @@ const cookieParser = require('cookie-parser');
 const express = require('express');
 const logger = require('morgan');
 const path = require('path');
+const userLogs = require('./middlewares/userLogs')
 
 // ************ express() - (don't touch) ************
 const app = express();
 
 // ************ Middlewares - (don't touch) ************
-app.use(express.static(path.join(__dirname, '../public')));  // Necesario para los archivos estáticos en el folder /public
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
+app.use(userLogs)
 
 // ************ Template Engine - (don't touch) ************
 app.set('view engine', 'ejs');
-app.set('views', './src/views'); // Seteo de la ubicación de la carpeta "views"
+app.set('views', './src/views');
 
 
 
 // ************ WRITE YOUR CODE FROM HERE ************
 // ************ Route System require and use() ************
 const mainRouter = require('./routes/main');
+const adminRouter = require('./routes/admin');
 app.use('/', mainRouter);
+app.use('/admin', adminRouter);
 
 
 
